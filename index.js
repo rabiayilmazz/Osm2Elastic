9353a25bdbdae572626c15d31795ddcea821210f
Rabia Yılmaz <yyilmazrabiaa@gmail.com>
2023-03-31 15:01:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,32 +6,42 @@ var jsdom = require("jsdom");
 var request = require("request");
 const fs = require('fs');
 
-var list = ["africa", "antarctica", "asia", "australia-oceania", "central-america", "europe", "north-america", "south-america"]
+var list = ["antarctica", "asia", "australia-oceania", "central-america", "europe", "north-america", "south-america"]
 
-for (let j = 0; j < list.length; j++) {
+    
 
-    url = "https://download.geofabrik.de/" + list[j] + ".html"
+function file(con){
+    
+    url = "https://download.geofabrik.de/"+con+".html"
 
-    // content of each file under decoder is retrieved
-    request(url, function(error, response, body) {
-        var results = new jsdom.JSDOM(body);
-
-        console.log(results.window.document.querySelectorAll('a'))
-
-        var countries = results.window.document.querySelectorAll('a')
-
-        for (var i = 0; i < countries.length; i++) {
-            var link = countries[i].getAttribute('href');
-            if (link.includes(list[i] + '/')) {
-
-                fs.appendFile(list[j], "wget https://download.geofabrik.de/" + link[i], function(err, data) {
-                    if (err) throw err;
-                    console.log(link[i]);
-                    console.log(list[j])
-
-                });
+        // content of each file under decoder is retrieved
+        request(url, function(error, response, body) {
+            var results = new jsdom.JSDOM(body);
+    
+            var countries = results.window.document.querySelectorAll('a')
+            for (var i = 0; i < countries.length; i++) {
+                var link = countries[i].getAttribute('href');
+                if (link.includes(con+"/") && link.includes('.osm.pbf')) {
+                    var command = "wget https://download.geofabrik.de/" + link
+    
+                    console.log(command)
+                    fs.appendFile("wget.sh",command+ "\n", function(err, data) {
+                        if (err) throw err;
+    
+                    });
+    
+                }
             }
-        }
-    });
+        });
+
 
 }
+
+file("antarctica")
+file("asia")
+file("australia-oceania")
+file("central-america")
+file("europe")
+file("north-america")
+file("south-america")
+
